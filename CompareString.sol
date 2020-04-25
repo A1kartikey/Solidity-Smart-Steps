3d9306010e73d313f8ef5e608f1d07109f8682a9
Kartikey <37590463+A1kartikey@users.noreply.github.com>
2020-04-26 00:34:37 +0530

    Update CompareString.sol

--- CompareString.sol
+++ CompareString.sol
@@ -1,7 +1,22 @@
-pragma solidity ^0.4.19;
+pragma experimental ABIEncoderV2;//This is Experimental features.Can not be used in production cases.
+pragma solidity ^0.5.7;
 
-contract MyContract {
-  function compare(string s1, string s2) public constant returns (bool) {
-    return (sha3(s1) == sha3(s2));
-  }
+contract stringComparision {
+    
+    string[] public names; //dynamic array of string 
+    
+ modifier checkNames (string memory _name){ // modifier to check existing name in array. 
+        
+        for(uint i = 0 ; i < names.length; i++){
+            
+            require (keccak256(abi.encodePacked(names[i])) != keccak256(abi.encodePacked(_name))); 
+                  // keccak256 computes the hash of the input.
+             }
+             
+        _; // go to the next line.
+    }
+ function setNames(string memory _name) public checkNames(_name){
+        
+        names.push(_name);
+    }
 }
